cd5897194c59f6ec64f807d265336bf487e89357
Pakeeza1508 <pakeezaislam1508@gmail.com>
2026-05-08 15:24:45 +0500

    On same page

--- db.php
+++ db.php
@@ -9,7 +9,7 @@ $database   = "shopping_db";
 /* CREATE CONNECTION */
 $conn = new mysqli($servername, $username, $password, $database);
 
-/* CHECK CONNECTION */
+/* CHECK CONNECTION */ 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
